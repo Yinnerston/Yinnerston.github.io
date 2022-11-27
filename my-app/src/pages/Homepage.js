6b14bcb954bc1fa5c2d033d292424155281d66c4
Nathan <nathanzjyin@gmail.com>
2022-11-27 19:13:55 +1100

    stretched background image across entire homepage and added first date entry on blog

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -2,28 +2,31 @@ import React from "react";
 import selfie from "assets/selfie.png"
 import background from "assets/background.png"
 import resume from "assets/resume.pdf"
-import { Col, Container, Image, Row } from "react-bootstrap";
 import "./Homepage.css"
 
 function Homepage() {
     return (
-        <div className="homepageDiv" style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat" }}>
+        <div className="homepageDiv" style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat", backgroundSize: "cover"}}>
             <h1>Looking for internships and junior dev positions.</h1>
-            <Container fluid >
-                <Row>
-                    <Col xs={6}>
-                        <p>I'm really really really interested in Data Engineering positions!!!</p>
-                    </Col>
-                    <Col xs={6}>
-                        <Image fluid={true} src={selfie} roundedCircle={true}></Image>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <embed src={resume} width="90%" height="1000vh" type="application/pdf" />
-                    </Col>
-                </Row>
-            </Container>
+            <div className="container">
+                <div className="textContainer">
+                    <h3>27/11/22</h3>
+                    <p>
+                       I discovered the field of Data Engineering a week ago and it looks really interesting!
+                    </p>
+                    <p>
+                        Right now, I'm working on a DE project using Docker, Terraform, Google Bigquery, dbt, Data Studio, Apache Spark and Airflow.
+                    </p>
+                    <p>
+                        Following the <a href="https://github.com/Yinnerston/data-engineering-zoomcamp">DataTalksClub Zoomcamp!</a>
+                    </p>
+                </div>
+                <img src={selfie} alt="Selfie"></img>
+            </div>
+            <br></br>
+            <h1>Resume:</h1>
+            <embed src={resume} width="90%" height="1300vh" type="application/pdf" />
+
         </div>
     );
 }
