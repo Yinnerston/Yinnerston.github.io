7121a509edfe92e9c8e1b8955b7cbe98b246c70f
Nathan Yin <nathanzjyin@gmail.com>
2022-12-06 13:21:12 +1100

    added todo to 6/12/22 blog post

--- my-app/src/components/BlogPostList.js
+++ my-app/src/components/BlogPostList.js
@@ -18,7 +18,11 @@ function BlogPostList() {
                     Likewise, using Airflow would have added costs and complexity beyond my use case, but I read the documentation and took notes <a href="https://docs.google.com/document/d/1dDA3epZMJDD8MFGUDYFgs9X5-l5_Es_mrqsgKdEM0m8/edit?usp=sharing">here</a>.
                 </p>
                 <p>
-                    I'll write a longer follow-up blog post on this, but you can view the dashboard I created <a href="https://datastudio.google.com/s/qd1dkgpHtms">here</a> and my code <a href="https://github.com/Yinnerston/data-engineering-zoomcamp">here</a>
+                    I'll write a longer follow-up blog post on this, but you can view the dashboard I created <a href="https://datastudio.google.com/s/qd1dkgpHtms">here</a> and my code <a href="https://github.com/Yinnerston/data-engineering-zoomcamp">here</a>.
+                </p>
+                <p>
+                    I'm planning on writing an injestion script to extract data from Twitter/Amazon and using Github Actions to deploy my code to a cloud function. Then to schedule a cronjob with google scheduler.
+                    I'm avoiding Airflow for this project due to the unnecessary complexity and costs it adds.
                 </p>
             </div>
             },
